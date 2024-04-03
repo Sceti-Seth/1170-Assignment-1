fe99601aa342cc09040a05a69716f838f9e2bc04
Sceti-Seth <sethachanning@icloud.com>
2024-04-03 10:21:50 -0300

    added allahu akbar

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sumForm = document.getElementById('sumForm');
-    const messageDiv = document.getElementById('message');
-
-    sumForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting normally
-        messageDiv.innerHTML = "Hello World"; // Display "Hello World" on the webpage
-    });
-});
+window.alert("allahu akbar");
