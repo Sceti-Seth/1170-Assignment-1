87e64ff487d5a04e4d59bc96b32ab65e1a33c9f2
Sceti-Seth <sethachanning@icloud.com>
2024-09-19 22:16:07 -0300

    done

--- script.js
+++ script.js
@@ -2,3 +2,56 @@
 //     window.alert("allahu akbar");
 // });
 
+
+
+function validateForm(){
+
+    const form= document.getElementById("ContactForm");
+    const submitButton= document.getElementById("submit-button");
+
+const message = document.getElementById("message-panel");
+
+submitButton.addEventListener("click", function validate(event)
+{
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phonenumber = document.getElementById("phonenumber").value;
+
+    const radio1 = document.getElementById("radio1").value;
+    const radio2=document.getElementById("radio2").value;
+
+    const usermessage=document.getElementById("usermessage").value;
+
+
+    if(!name){
+        message.innerHTML += "Please input your name <br>";
+        message.style.color="red";
+    }
+
+    else if(!email){
+        message.innerHTML += "Please input your email <br>";
+        message.style.color="red";
+    }
+    else if(!(email.endsWith("dal.ca"))){
+        message.innerHTML += "Not a valid dal email <br>";
+        message.style.color = "red";
+    }
+
+    else if(!phonenumber){
+        message.innerHTML += "Please input your phone number <br>";
+        message.style.color="red";
+    }
+
+    else if(!(radio1) && !(radio2)){
+        message.innerHTML += "Please select either Student or TA <br>";
+        message.style.color ="red";
+    }
+
+}
+
+)
+}
+
+validateForm();
